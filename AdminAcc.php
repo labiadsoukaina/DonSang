36b5c8170b3c5141fc85a08f2ec67cddc894511f
Hafsazn <36314849+Hafsazn@users.noreply.github.com>
2018-05-18 16:53:17 +0000

    Add files via upload

---
+++ AdminAcc.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+    
+    <?php
+    
+    /*include ('connexion.DB');
+    $x= "SELECT * FROM personnel";
+    echo $x;   */ 
+    ?>
+       
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>FrstPage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="AdminAcc.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+     <div class="header"></div>
+     
+    <nav>
+        <ul>
+            <li><a  href="AdminAcc.php" >Gestion des plannings</a></li>
+            <li><a href="Personnel.php">Gestion du personnel</a></li>
+            <li><a href="Don.php">Gestion des dons</a></li>
+            <li><a href="affichersalle.php">Gestion des salles</a></li>
+        </ul>
+    </nav>
+    
+
+
+    
+</body>
+</html>
